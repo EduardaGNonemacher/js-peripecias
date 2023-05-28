523a724aca1798db30b1892cea8566b970dc7449
KwidRebaixada <103470086+KwidRebaixada@users.noreply.github.com>
2023-05-28 11:47:51 -0300

    1 file modified

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ console.log("Retorna o valor máximo dos números dados, nesse caso sendo o núm
 let Mínimo = Math.min(1, 3, 6, 23);
 console.log(" Retorna o valor mínimo dos números informados, nesse caso sendo o número: ", Mínimo);
 
-let pou = Math.pow();
+let pou = Math.pow(5,8);
 console.log("Retorna o valor da base elevado ao expoente, nesse caso sendo o número: ", pou);
 
+let Random = Math.random();
+console.log(" Retorna um número aleatório entre 0 e 1, nesse caso sendo o número: ",Random);
 
+let SQ = Math.sqrt(2);
+console.log("Retorna a raiz quadrada do número dado, nesse caso sendo o número: ", SQ);
 
