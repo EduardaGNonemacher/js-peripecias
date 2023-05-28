09821d81023005ccaebc8b2af64c97dab4e4f33c
KwidRebaixada <103470086+KwidRebaixada@users.noreply.github.com>
2023-05-28 11:33:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-peripecias
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-muxdru)
---
+++ index.js
@@ -0,0 +1,6 @@
+let Cosseno = Math.cos(28);
+console.log(Cosseno);
+
+let Maximo = Math.max(1, 3, 6, 8);
+console.log(Maximo);
+
