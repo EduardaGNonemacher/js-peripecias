eb1c4225878c547b67e5591b20b7c377101d9fc9
KwidRebaixada <103470086+KwidRebaixada@users.noreply.github.com>
2023-05-28 11:43:25 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 let Cosseno = Math.cos(28);
-console.log(Cosseno);
+console.log("Retorna o cosseno do número informado, nesse caso sendo o número: ",Cosseno);
 
 let Maximo = Math.max(1, 3, 6, 8);
-console.log(Maximo);
+console.log("Retorna o valor máximo dos números dados, nesse caso sendo o número: ", Maximo);
+
+let Mínimo = Math.min(1, 3, 6, 23);
+console.log(" Retorna o valor mínimo dos números informados, nesse caso sendo o número: ", Mínimo);
+
+let pou = Math.pow();
+console.log("Retorna o valor da base elevado ao expoente, nesse caso sendo o número: ", pou);
+
+
 
